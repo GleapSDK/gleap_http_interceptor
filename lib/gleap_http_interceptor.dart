3f82143dc8c0d8fc2b52297cbd1ffc380d9844e4
Tobias Duelli <duellit@gmail.com>
2022-06-14 09:16:23 +0200

    updated to registerListener

--- lib/gleap_http_interceptor.dart
+++ lib/gleap_http_interceptor.dart
@@ -6,16 +6,18 @@ import 'package:gleap_sdk/models/gleap_network_log_models/gleap_network_log_mode
 import 'package:gleap_sdk/models/gleap_network_log_models/gleap_network_request_model/gleap_network_request_model.dart';
 import 'package:gleap_sdk/models/gleap_network_log_models/gleap_network_response_model/gleap_network_response_model.dart';
 import 'package:gleap_sdk/models/ring_buffer_model/ring_buffer_model.dart';
-import 'package:http_interceptor/http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 class GleapHttpInterceptor implements InterceptorContract {
   RingBuffer<GleapNetworkLog> networkLogs = RingBuffer<GleapNetworkLog>(20);
 
   GleapHttpInterceptor() {
-    Gleap.setFeedbackWillBeSentCallback(callbackHandler: () {
-      Gleap.attachNetworkLogs(networkLogs: networkLogs.toList());
-    });
+    Gleap.registerListener(
+      actionName: 'feedbackFlowStarted',
+      callbackHandler: (_) {
+        Gleap.attachNetworkLogs(networkLogs: networkLogs.toList());
+      },
+    );
   }
 
   @override
